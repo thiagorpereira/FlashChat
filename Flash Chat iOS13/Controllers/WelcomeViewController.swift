056db1abdbec265c27ca9ecf4df818ebbb14489c
Thiago Pereira <thiagorpereira7@gmail.com>
2023-04-27 19:51:35 -0300

    feat: set animation in first screen and navigations

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -15,7 +15,18 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       
+        titleLabel.text = ""
+        var charIndex: Double = 0
+        let titleText = "⚡FlashChat"
+        for letter in titleText {
+            print(charIndex)
+            print(letter)
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+
+        }
     }
     
 
